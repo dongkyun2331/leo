41eb0411f19f33fe2d89b58a3b88615f42571449
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-12-10 12:25:43 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+PRIVATEKEY="${PRIVATEKEY}"
+APPNAME="<pori>"
+
+cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://api.explorer.aleo.org/v1" --path "./${APPNAME}/build/" --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" --priority-fee 0
