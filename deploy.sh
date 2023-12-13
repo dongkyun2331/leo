1ebae7f6d98ea59032aff3e2f3237318d41b1c16
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-12-14 03:34:34 +0900

    deploy,sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 PRIVATEKEY="${PRIVATEKEY}"
 APPNAME="<pori>"
 
-cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://api.explorer.aleo.org/v1" --path "./${APPNAME}/build/" --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" --priority-fee 0
+cd .. && snarkos developer deploy "pori_4335.aleo" --private-key "APrivateKey1zkp33teKFYRsJ9zqTVU9kFCzXoj1HjfhHhMoW6248hMDZPw" --query "https://api.explorer.aleo.org/v1" --path "./pori_4335/build/" --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" --priority-fee 0
